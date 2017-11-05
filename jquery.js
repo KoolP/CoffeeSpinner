6d251bfd5707794ea6a9c511528fa7ae30cc8415
KoolP <patrik.rikama@gmail.com>
2017-11-05 17:38:56 +0100

    Added a jQuery library, tested some effects for navbar but commented them out.

---
+++ jquery.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+//Basic selectors, just like css selectors
+  //$('*').css('border', '4px solid black');
+//Basic animation
+//  $('.tab').slideDown("slow");
+
+
+});
