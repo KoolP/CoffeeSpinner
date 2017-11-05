2ba742bb3dbf93a8a4034195ff41984a0f515119
KoolP <patrik.rikama@gmail.com>
2017-11-05 12:46:40 +0100

    Cleaned the js code page

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
 
-//
-//Version 1.0
-//Alerts winner randomly from array List
-/*
-function coffeySpinner() {
-    //document.getElementById("button").innerHTML =
-    winner =  ['Aronija', 'August', 'Christian', 'Emma', 'Hanna', 'Jan', 'Mani', 'Patrik', 'Ria', 'William'];
-    var luckyWinner = Math.floor(Math.random() * winner.length);
-    alert(winner[luckyWinner]);
-}*/
 
-//VERSION 1.2,
-//Changelog: Opens div of randomized luckywinner with picture, name etc.
-/*
-function coffeySpinner() {
-    var teamList = ['bill', 'steve', 'linus', 'mark'];
-    var random = Math.floor(Math.random() * teamList.length)
-    var showWinner = document.getElementById(teamList[random]);
-    showWinner.className = "show";
-}*/
 
-//Contact function
+//Contact alert function
 function contact() {
   alert("Hello! App produced by koolsport.se and friends");
 }
 
-//TEST Toggle. Works but the randomizer take a new person the old stays
-//21.9 Not working try adding eventlistner instead of onclick
-
+//The randomizer with an list of participants
 function coffeySpinner() {
     var allCards = document.querySelectorAll(".show");
     for (var i = 0; i < allCards.length; i++) {
@@ -45,40 +24,22 @@ function coffeySpinner() {
     } else {
       showWinner.className = "hide";
     }
-    //har testat ifelse,hide/show x2, location reload
 
-    /*
-    if(showWinner.className === "show") {
-      showWinner.className = "hide";
-    } else {
-      showWinner.className = "hide";
-    }*/
 }
 
-/*
-Version 1.4
-Changelog: Array outside of function, otherwise it runs it again and again each time you push if not reloaded. ads teamList.splice to take away chosen var random from list, Includes If Else to to inform console to stop after array empty.
 
-var teamList = ['bill', 'steve', 'linus', 'mark'];
 
-function coffeySpinner() {
-  if(teamList.length > 0){
-    var random = Math.floor(Math.random() * teamList.length)
-    //TEST: alert(teamList[luckyWinner]); //Gives name of teamlist, actually it is a number
-    var showWinner = document.getElementById(teamList[random]);
-    //Takes away chosen one from list:
-    teamList.splice(random, 1)
+//TEST Toggle. Works but the randomizer take a new person the old stays
+//21.9 Not working try adding eventlistner instead of onclick
 
-    console.log(teamList)
-    showWinner.className = "show";
+/*tested ifelse,hide/show x2, location reload
+if(showWinner.className === "show") {
+  showWinner.className = "hide";
+} else {
+  showWinner.className = "hide";
+}*/
 
-  } else {
-    console.log('slut på personer!')
-  }
-}
-*/
-
-//NOTES AND LEARNED DURING PROJECT (ITHS 2017 teacher:jon mates:andreas,oliver)
+//NOTES AND LEARNED DURING PROJECT (ITHS 2017 jon, andreas,oliver)
 //Read code with falcon eyes, see the mistakes.
 //Use inspect element console
 //console.log(showWinner);
