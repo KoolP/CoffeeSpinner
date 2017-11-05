66e40918375871796d71a55dc6392789491a9257
KoolP <patrik.rikama@gmail.com>
2017-11-05 23:37:46 +0100

    Changed the headers for each theme

--- jquery.js
+++ jquery.js
@@ -9,24 +9,28 @@ $(document).ready(function() {
 $('#coffeebrew').click(function(){
   $('body').css("background", "url(media/kahvia.jpg)");
   $('#banner').css("background-color", "red");
+  $('#bannerheader').text("Coffee Spinner");
   $('#bannerheadertwo').text("Who brews coffee?");
 });
 
 $('#dishwashing').click(function(){
   $('body').css("background", "url(media/dishwash.jpg)");
   $('#banner').css("background-color", "green");
+  $('#bannerheader').text("Dishwash Spinner");
   $('#bannerheadertwo').text("Who washes dishes?");
 });
 
 $('#garbages').click(function() {
   $('body').css("background", "url(media/garbage.jpg)");
   $('#banner').css("background-color", "blue");
+  $('#bannerheader').text("Garbage Spinner");
   $('#bannerheadertwo').text("Who empties the bin?");
 });
 
 $('#cooking').click(function() {
   $('body').css("background", "url(media/food.jpg)")
   $('#banner').css("background-color", "purple");
+  $('#bannerheader').text("Cooking Spinner");
   $('#bannerheadertwo').text("Who cooks food?");
 });
 
