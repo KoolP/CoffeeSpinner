c7c864d8c45aa2b68f9fe786ab6bc2312a71dd41
KoolP <patrik.rikama@gmail.com>
2017-11-05 22:47:34 +0100

    Bacgrounds added for the themes

--- jquery.js
+++ jquery.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
 //Basic selectors, just like css selectors
   //$('*').css('border', '4px solid black');
-//Basic animation
+//Basic animation slider that almost worked
 //  $('.tab').slideDown("slow");
+
+
+//Change themes
+$('#coffeebrew').click(function(){
+  $('body').css("background", "url(media/kahvia.jpg)");
+});
+
+$('#dishwashing').click(function(){
+  $('body').css("background", "url(media/dishwash.jpg)");
+});
+
+$('#garbages').click(function() {
+  $('body').css("background", "url(media/garbage.jpg)");
+});
+
+$('#cooking').click(function() {
+  $('body').css("background", "url(media/food.jpg)")
+});
 
 
 });
