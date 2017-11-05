36614bfb9f7593081080028b37f4cde4367668b4
KoolP <patrik.rikama@gmail.com>
2017-11-05 23:32:02 +0100

    Changed text and added text change to theme changes.

--- jquery.js
+++ jquery.js
@@ -9,21 +9,25 @@ $(document).ready(function() {
 $('#coffeebrew').click(function(){
   $('body').css("background", "url(media/kahvia.jpg)");
   $('#banner').css("background-color", "red");
+  $('#bannerheadertwo').text("Who brews coffee?");
 });
 
 $('#dishwashing').click(function(){
   $('body').css("background", "url(media/dishwash.jpg)");
   $('#banner').css("background-color", "green");
+  $('#bannerheadertwo').text("Who washes dishes?");
 });
 
 $('#garbages').click(function() {
   $('body').css("background", "url(media/garbage.jpg)");
   $('#banner').css("background-color", "blue");
+  $('#bannerheadertwo').text("Who empties the bin?");
 });
 
 $('#cooking').click(function() {
   $('body').css("background", "url(media/food.jpg)")
   $('#banner').css("background-color", "purple");
+  $('#bannerheadertwo').text("Who cooks food?");
 });
 
 
